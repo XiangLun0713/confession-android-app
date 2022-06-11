af8bef6318d8147466831f6e8ba19a96bcdacd08
CHOO XIANG LUN <xlchoo25@gmail.com>
2022-06-12 00:13:31 +0800

    Minor fixing Jiajun's code - Fix Android Manifest file merge conflict - Fix Sign up page responsiveness issues - Delete useless Profile page - move the location of the User.java - remove progress bar variable from LoginActivity.java

--- app/src/main/java/me/xianglun/confession_app/SignUpActivity.java
+++ app/src/main/java/me/xianglun/confession_app/SignUpActivity.java
@@ -1,17 +1,16 @@
 package me.xianglun.confession_app;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Patterns;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -19,9 +18,11 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
 
-import org.w3c.dom.Text;
+import java.util.Objects;
 
-public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
+import me.xianglun.confession_app.model.User;
+
+public class SignUpActivity extends AppCompatActivity {
 
     //initialize some variables
     private TextView banner, registerUser;
@@ -36,35 +37,14 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
         setContentView(R.layout.activity_sign_up);
 
         mAuth = FirebaseAuth.getInstance();
+        banner = findViewById(R.id.banner);
+        registerUser = findViewById(R.id.registerUser);
+        editTextEmail = findViewById(R.id.email);
+        editTextPassword = findViewById(R.id.password);
+        progressBar = findViewById(R.id.progressBar);
 
-        banner = (TextView) findViewById(R.id.banner);
-        banner.setOnClickListener(this);
-
-        registerUser = (Button) findViewById(R.id.registerUser);
-        registerUser.setOnClickListener(this);
-
-        editTextEmail = (EditText) findViewById(R.id.email);
-        editTextPassword = (EditText) findViewById(R.id.password);
-
-        progressBar = (ProgressBar) findViewById(R.id.progressBar);
-
-
-
-
-    }
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.banner:
-                startActivity(new Intent(this, MainActivity.class));
-                break;
-            case R.id.registerUser:
-                registerUser();
-                startActivity(new Intent(this, LoginActivity.class));
-                break;
-
-        }
+        registerUser.setOnClickListener(v -> registerUser());
+        banner.setOnClickListener(v -> startActivity(new Intent(SignUpActivity.this, MainActivity.class)));
     }
 
     //register user through firebase
@@ -72,25 +52,25 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
         String email = editTextEmail.getText().toString().trim();
         String password = editTextPassword.getText().toString().trim();
 
-        if(email.isEmpty()){
+        if (email.isEmpty()) {
             editTextEmail.setError("Email is required!");
             editTextEmail.requestFocus();
             return;
         }
 
-        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
             editTextEmail.setError("Please provide valid email!");
             editTextEmail.requestFocus();
             return;
         }
 
-        if(password.isEmpty()){
+        if (password.isEmpty()) {
             editTextPassword.setError("Password is required!");
             editTextPassword.requestFocus();
             return;
         }
 
-        if(password.length() < 6){
+        if (password.length() < 6) {
             editTextPassword.setError("Min password length should be 6 characters!");
             editTextPassword.requestFocus();
             return;
@@ -99,35 +79,30 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
         //method for creating new user through firebase
         progressBar.setVisibility(View.VISIBLE);
         mAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
+                .addOnCompleteListener(task -> {
 
-                        if(task.isSuccessful()){
-                            User user = new User(email);
+                    if (task.isSuccessful()) {
+                        User user = new User(email);
 
-                            FirebaseDatabase.getInstance().getReference("Users")
-                                    .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
-                                    .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
-                                        @Override
-                                        public void onComplete(@NonNull Task<Void> task) {
+                        FirebaseDatabase.getInstance().getReference("Users")
+                                .child(Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid())
+                                .setValue(user).addOnCompleteListener(task1 -> {
+                                    if (task1.isSuccessful()) {
+                                        progressBar.setVisibility(View.INVISIBLE);
+                                        Toast.makeText(SignUpActivity.this, "You has been registered successfully!", Toast.LENGTH_LONG).show();
+                                        startActivity(new Intent(SignUpActivity.this, LoginActivity.class));
 
-                                            if(task.isSuccessful()){
-                                                Toast.makeText(SignUpActivity.this, "User has been registered successfully!", Toast.LENGTH_LONG).show();
-                                                progressBar.setVisibility(View.INVISIBLE);
+                                        // redirect to Login Layout!
+                                    } else {
+                                        Toast.makeText(SignUpActivity.this, "Failed to register! Try again!", Toast.LENGTH_LONG).show();
+                                        progressBar.setVisibility(View.GONE);
+                                    }
+                                });
 
-                                                // redirect to Login Layout!
-                                            }else{
-                                                Toast.makeText(SignUpActivity.this, "Failed to register! Try again!", Toast.LENGTH_LONG).show();
-                                                progressBar.setVisibility(View.GONE);
-                                            }
-                                        }
-                                    });
 
-                        }else{
-                            Toast.makeText(SignUpActivity.this, "Failed to register! Try again!", Toast.LENGTH_LONG).show();
-                            progressBar.setVisibility(View.GONE);
-                        }
+                    } else {
+                        Toast.makeText(SignUpActivity.this, "Failed to register! Try again!", Toast.LENGTH_LONG).show();
+                        progressBar.setVisibility(View.GONE);
                     }
                 });
     }
